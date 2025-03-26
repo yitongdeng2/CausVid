dea481b33accc0731b52046524456fae3cb35794
Xun Huang <xunhuang1995@gmail.com>
2025-03-25 20:18:05 -0400

    fix a bug in sampling script

--- minimal_inference/longvideo_autoregressive_inference.py
+++ minimal_inference/longvideo_autoregressive_inference.py
@@ -70,7 +70,7 @@ for prompt_index in tqdm(range(len(dataset))):
 
         current_video = video[0].permute(0, 2, 3, 1).cpu().numpy()
 
-        start_frame = encode(pipeline.vae, (video[:, :, -9:-8] * 2.0 - 1.0).to(
+        start_frame = encode(pipeline.vae, (video[:, -9:-8, :] * 2.0 - 1.0).transpose(2, 1).to(
             torch.bfloat16)).transpose(2, 1).to(torch.bfloat16)
 
         start_latents = torch.cat(
